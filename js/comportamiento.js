3e78213ea438d18ca4ba5838513031866c6d1951
Ana Iñiguez <anitandil@yahoo.com>
2023-02-16 13:24:38 -0300

    cambio de color en hover de enlaces

--- js/comportamiento.js
+++ js/comportamiento.js
@@ -5,6 +5,7 @@ let contacto = document.getElementById('contactame');
 contactar.addEventListener('click', function() {
 	contacto.style="background-color: rgba(158,200,200,0.9)";
 document.getElementById('contactomail').style.display ="block";
+document.getElementById('nombre').autofocus;
 });
 
 document.getElementById("enviarEmail").addEventListener('click', function() {
